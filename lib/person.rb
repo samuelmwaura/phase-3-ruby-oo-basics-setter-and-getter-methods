00ef6f87d4c28777a54e0ce64c5b99249a946957
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-18 01:53:29 +0300

    commit tree 68591d4d32f6966f04ad651fde51eb293d66c402

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
+  def name=(name) #nb// the equal sign is part of the method name and serves to indicate that the method is a setter
+    @name=name
+  end
 
+  def job=(job)
+    @job= job
+  end
+
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
 end
